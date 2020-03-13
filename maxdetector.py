dee3f4a339d5bd684baad1e55e5660059d8c5bff
Lars Kellogg-Stedman <lars@oddbit.com>
2020-03-13 19:21:47 -0400

    move alarm code over to arduino
    the python code now handles only ssid scanning and raising a signal
    when a target bssid is found.

--- maxdetector.py
+++ maxdetector.py
@@ -8,30 +8,21 @@ from machine import Timer
 
 
 class Monitor(object):
-    def __init__(self, targets, pin_bell=5, pin_button=0):
+    def __init__(self, targets, pin_bell=5):
         self.targets = targets
-        self.button = Pin(pin_button, Pin.IN, Pin.PULL_UP)
-        self.bell = Pin(pin_bell, Pin.OUT)
-        self.pwm = PWM(self.bell)
+        self.signal = Pin(pin_bell, Pin.OUT)
         self.t_scan = None
-        self.t_monitor = None
         self.nic = network.WLAN(network.STA_IF)
-        self.found = False
 
     def start(self):
         print('Starting...')
         self.t_scan = Timer(-1)
-        self.t_monitor = Timer(-1)
         self.t_scan.init(period=10000, mode=Timer.PERIODIC,
                          callback=lambda t: self.scan())
-        self.t_monitor.init(period=100, mode=Timer.PERIODIC,
-                            callback=lambda t: self.monitor())
         print('Started.')
 
     def stop(self):
         print('Stopping...')
-        self.t_monitor.deinit()
-        self.t_monitor = None
         self.t_scan.deinit()
         self.t_scan = None
         print('Stopped.')
@@ -48,25 +39,9 @@ class Monitor(object):
             ))
             if bssid in targets:
                 print('Found ssid "{}" bssid "{}"'.format(ssid, _bssid))
-                self.found = True
+                self.signal.value(1)
                 break
         else:
-            self.found = False
+            self.signal.value(0)
 
         print('Finished scanning t={}'.format(time.time()))
-
-    def monitor(self):
-        pwm = self.pwm
-
-        if self.found:
-            print('ALARM ALARM')
-            pwm.freq(500)
-            pwm.duty(512)
-            for i in range(2):
-                pwm.freq(500)
-                time.sleep(0.5)
-                pwm.freq(800)
-                time.sleep(0.5)
-
-            pwm.duty(0)
-            self.found = False
