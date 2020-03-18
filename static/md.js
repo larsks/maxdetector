d1c1a5f336491da5305cd2dd02b3dde9eee4da85
Lars Kellogg-Stedman <lars@oddbit.com>
2020-03-18 11:14:25 -0400

    add notification is device is unreachable
    show a banner at the top of the page if the device is not
    responding to api requests.

--- static/md.js
+++ static/md.js
@@ -1,22 +1,48 @@
+var running = document.getElementById("running");
+var alarm = document.getElementById("alarm");
+var button_start = document.getElementById("button_start");
+var button_stop = document.getElementById("button_stop");
+var message = document.getElementById("message");
+
+function show_message(msg) {
+    message.innerHTML = msg;
+    message.style.display = "block";
+}
+
+function clear_message() {
+    message.style.display = "none";
+}
+
+function xhr_failed(xhr, status) {
+    show_message(`Failed to get status from detector (${status}).`);
+}
+
 function update_page() {
-    var running = document.getElementById("running");
-    var alarm = document.getElementById("alarm");
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            var status = JSON.parse(this.responseText);
-            running.innerHTML = status.running;
-            alarm.innerHTML = status.alarm;
+            if (xhr.status == 200) {
+                var status = JSON.parse(this.responseText);
+                clear_message();
+                running.innerHTML = status.running;
+                alarm.innerHTML = status.alarm;
+            } else {
+                xhr_failed(xhr, xhr.status);
+            }
         }
     }
+    xhr.onerror = function() {
+        xhr_failed(xhr, xhr.status);
+    }
+
+    // Needs to be > 2 seconds because that's approximately how
+    // long a wifi scan takes.
+    xhr.timeout = 5000;
 
     xhr.open("GET", "/api/status");
     xhr.send();
 }
-
-var button_start = document.getElementById("button_start");
-var button_stop = document.getElementById("button_stop");
 
 button_start.addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
