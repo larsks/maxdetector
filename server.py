15e9b4bb11d5ea4f734a7d1e511fb9da47f2574a
Lars Kellogg-Stedman <lars@oddbit.com>
2020-04-05 08:26:40 -0400

    Refactor server implementation
    This splits out generic http server methods into BaseServer, and folds
    the API class into Server. This makes the organization a little more
    obvious and means that we can steal BaseServer verbatim for reuse in
    other projects.

--- server.py
+++ server.py
@@ -74,8 +74,248 @@ def map_content_type(filename):
     return EXTENSIONS.get(ext, "application/octet-stream")
 
 
-class API(object):
-    '''Mix-in class that provides the API method implementations.'''
+class BaseServer(object):
+    def __init__(self, port=80, poll_interval=1000):
+        self.port = port
+        self.poll_interval = poll_interval
+        self.routes = []
+        self.running = False
+
+    def register(self, path, handler, method="GET"):
+        '''Associate a handler function with a route'''
+
+        self.routes.append(
+            {
+                "path": path,
+                "re": re.compile(path),
+                "handler": handler,
+                "method": method,
+            }
+        )
+
+    def lookup_route(self, req):
+        '''Find a registered route to handle the given request'''
+
+        for route in self.routes:
+            if route["method"] != req.method:
+                continue
+
+            match = route["re"].match(req.path)
+            if match:
+                print(
+                    "method {} path {} matched {}".format(
+                        req.method, req.path, route["path"],
+                    )
+                )
+                break
+        else:
+            route = None
+            match = None
+
+        return route, match
+
+    def handle_request(self, client, req):
+        '''Handle a client request and return a Response'''
+
+        route, match = self.lookup_route(req)
+        if route is None:
+            raise KeyError("No handler for {}\n".format(req.path))
+
+        res = route["handler"](client, req, match)
+        if not isinstance(res, Response):
+            res = Response(200, None, res)
+
+        return res
+
+    def send_response(self, client, res, req):
+        '''Send a response to the client'''
+
+        nb = client.write(
+            "HTTP/1.1 {} {}\r\n".format(
+                res.status_code, STATUS_MESSAGE.get(res.status_code, "UNKNOWN")
+            )
+        )
+
+        if isinstance(res.content, (dict, list)):
+            nb += client.write(
+                "Content-type: {}\r\n\r\n".format(
+                    res.content_type or "application/json"
+                )
+            )
+            nb += client.write(json.dumps(res.content))
+        elif res.content is None:
+            pass
+        elif hasattr(res.content, "read"):
+            nb += client.write(
+                "Content-type: {}\r\n\r\n".format(res.content_type or "text/html")
+            )
+            nb += self.send_file(client, res.content)
+        else:
+            nb += client.write(
+                "Content-type: {}\r\n\r\n".format(res.content_type or "text/html")
+            )
+            nb += client.write(res.content)
+
+        return nb
+
+    def send_file(self, client, fd):
+        '''Send content from a file to the given client'''
+
+        size = 0
+        while True:
+            nb = fd.readinto(buf)
+            size += nb
+            if nb == 0:
+                break
+            client.write(buf[:nb])
+
+        fd.close()
+        return size
+
+    def read_request(self, client, addr):
+        '''Read a request from the client and return a Request object'''
+
+        state = 0
+        clen = None
+        params = {}
+
+        while True:
+            if state == 0:
+                # Read request line
+                line = client.readline()
+                method, path, version = line.decode().split()
+                try:
+                    path, qs = path.split('?', 1)
+                    params.update(parse_qs(qs))
+                except ValueError:
+                    pass
+                state = 1
+            elif state == 1:
+                # Read headers
+                line = client.readline()
+
+                if line and line != b"\r\n":
+                    header, value = line.decode().split(": ", 1)
+                    if header.lower() == "content-length":
+                        clen = int(value.strip())
+                else:
+                    if method in ["GET", "DELETE"]:
+                        break
+                    elif method in ["PUT", "POST"]:
+                        state = 2
+                    else:
+                        raise ValueError(method)
+            elif state == 2:
+                # Read request body
+                content = client.read(clen)
+                params.update(parse_qs(content.decode()))
+                break
+
+        return Request(method, path, version, params)
+
+    def start(self):
+        s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(("", self.port))
+        s.listen(5)
+        self.sock = s
+        self.running = True
+        print('Starting server.')
+
+        try:
+            self.loop()
+        finally:
+            self.stop()
+
+    def idle(self):
+        pass
+
+    def loop(self):
+        s = self.sock
+
+        poll = select.poll()
+        poll.register(s, select.POLLIN)
+
+        while True:
+            events = poll.poll(self.poll_interval)
+
+            self.idle()
+
+            if not events:
+                continue
+
+            client, addr = s.accept()
+            print("Client connected from {}.".format(addr[0]))
+
+            try:
+                req = self.read_request(client, addr)
+                res = self.handle_request(client, req)
+            except Exception as err:
+                if isinstance(err, KeyError):
+                    status_code = 404
+                else:
+                    status_code = 500
+
+                print("ERROR: Failed handling request from {}: {}: {}"
+                      .format(addr[0], type(err), err))
+                res = Response(status_code, "text/html",
+                               "An unexpected error has occurred: {}\n".format(err))
+
+            try:
+                size = self.send_response(client, res, req)
+            except Exception as err:
+                print("ERROR: Failed sending response to {}: {}".format(addr[0], err))
+                print(
+                    '{} - - [{}] "{} {} {}" {} {}'.format(
+                        addr[0],
+                        time.time(),
+                        req.method,
+                        req.path,
+                        req.version,
+                        res.status_code,
+                        size,
+                    )
+                )
+
+            print("Closing connection from {}".format(addr[0]))
+            client.close()
+
+    def stop(self):
+        if self.sock is not None:
+            print("Closing server socket.")
+            self.sock.close()
+        self.sock = None
+        self.running = False
+
+
+class Server(BaseServer):
+    def __init__(self, mdo, port=80):
+        super().__init__(port=port)
+
+        self.mdo = mdo
+        self.add_routes()
+
+    def add_routes(self):
+        '''Register HTTP route handlers'''
+
+        self.register("/api/target$", self.api_list_targets, method="GET")
+        self.register("/api/target$", self.api_add_target, method="POST")
+        self.register("/api/target/([^/]*)$", self.api_delete_target, method="DELETE")
+        self.register("/api/status$", self.api_status)
+        self.register("/api/scan$", self.api_scan_status)
+        self.register("/api/scan$", self.api_scan_control, method="POST")
+        self.register("/api/scan/results?$", self.api_scan_results)
+        self.register("/api/silent$", self.api_silent_status)
+        self.register("/api/silent$", self.api_silent_control, method="POST")
+        self.register("/api/memory$", self.api_memory)
+        self.register("/api/wifi", self.api_wifi, method="POST")
+        self.register("/api/reset", self.api_reset)
+        self.register("/static/(.*)$", self.static_content)
+        self.register("/$", self.index)
+
+    def index(self, *args):
+        '''Return web UI'''
+        return Response(200, "text/html", open("ui.html"))
 
     def api_memory(self, client, req, match):
         '''Return heap statistics from gc module'''
@@ -197,239 +437,3 @@ class API(object):
                callback=lambda t: self._reset_post())
 
         return Response(200, "text/html", "Resetting\n")
-
-
-class Server(API):
-    def __init__(self, mdo, port=80):
-        self.mdo = mdo
-        self.port = port
-        self.routes = []
-        self.running = False
-
-        self.add_routes()
-
-    def add_routes(self):
-        '''Register HTTP route handlers'''
-
-        self.register("/api/target$", self.api_list_targets, method="GET")
-        self.register("/api/target$", self.api_add_target, method="POST")
-        self.register("/api/target/([^/]*)$", self.api_delete_target, method="DELETE")
-        self.register("/api/status$", self.api_status)
-        self.register("/api/scan$", self.api_scan_status)
-        self.register("/api/scan$", self.api_scan_control, method="POST")
-        self.register("/api/scan/results?$", self.api_scan_results)
-        self.register("/api/silent$", self.api_silent_status)
-        self.register("/api/silent$", self.api_silent_control, method="POST")
-        self.register("/api/memory$", self.api_memory)
-        self.register("/api/wifi", self.api_wifi, method="POST")
-        self.register("/api/reset", self.api_reset)
-        self.register("/static/(.*)$", self.static_content)
-        self.register("/$", self.index)
-
-    def register(self, path, handler, method="GET"):
-        '''Associate a handler function with a route'''
-
-        self.routes.append(
-            {
-                "path": path,
-                "re": re.compile(path),
-                "handler": handler,
-                "method": method,
-            }
-        )
-
-    def lookup_route(self, req):
-        '''Find a registered route to handle the given request'''
-
-        for route in self.routes:
-            if route["method"] != req.method:
-                continue
-
-            match = route["re"].match(req.path)
-            if match:
-                print(
-                    "method {} path {} matched {}".format(
-                        req.method, req.path, route["path"],
-                    )
-                )
-                break
-        else:
-            route = None
-            match = None
-
-        return route, match
-
-    def handle_request(self, client, req):
-        '''Handle a client request and return a Response'''
-
-        route, match = self.lookup_route(req)
-        if route is None:
-            raise KeyError("No handler for {}\n".format(req.path))
-
-        res = route["handler"](client, req, match)
-        if not isinstance(res, Response):
-            res = Response(200, None, res)
-
-        return res
-
-    def send_response(self, client, res, req):
-        '''Send a response to the client'''
-
-        nb = client.write(
-            "HTTP/1.1 {} {}\r\n".format(
-                res.status_code, STATUS_MESSAGE.get(res.status_code, "UNKNOWN")
-            )
-        )
-
-        if isinstance(res.content, (dict, list)):
-            nb += client.write(
-                "Content-type: {}\r\n\r\n".format(
-                    res.content_type or "application/json"
-                )
-            )
-            nb += client.write(json.dumps(res.content))
-        elif res.content is None:
-            pass
-        elif hasattr(res.content, "read"):
-            nb += client.write(
-                "Content-type: {}\r\n\r\n".format(res.content_type or "text/html")
-            )
-            nb += self.send_file(client, res.content)
-        else:
-            nb += client.write(
-                "Content-type: {}\r\n\r\n".format(res.content_type or "text/html")
-            )
-            nb += client.write(res.content)
-
-        return nb
-
-    def send_file(self, client, fd):
-        '''Send content from a file to the given client'''
-
-        size = 0
-        while True:
-            nb = fd.readinto(buf)
-            size += nb
-            if nb == 0:
-                break
-            client.write(buf[:nb])
-
-        fd.close()
-        return size
-
-    def index(self, *args):
-        return Response(200, "text/html", open("ui.html"))
-
-    def read_request(self, client, addr):
-        '''Read a request from the client and return a Request object'''
-
-        state = 0
-        clen = None
-        params = {}
-
-        while True:
-            if state == 0:
-                # Read request line
-                line = client.readline()
-                method, path, version = line.decode().split()
-                try:
-                    path, qs = path.split('?', 1)
-                    params.update(parse_qs(qs))
-                except ValueError:
-                    pass
-                state = 1
-            elif state == 1:
-                # Read headers
-                line = client.readline()
-
-                if line and line != b"\r\n":
-                    header, value = line.decode().split(": ", 1)
-                    if header.lower() == "content-length":
-                        clen = int(value.strip())
-                else:
-                    if method in ["GET", "DELETE"]:
-                        break
-                    elif method in ["PUT", "POST"]:
-                        state = 2
-                    else:
-                        raise ValueError(method)
-            elif state == 2:
-                # Read request body
-                content = client.read(clen)
-                params.update(parse_qs(content.decode()))
-                break
-
-        return Request(method, path, version, params)
-
-    def start(self):
-        s = socket.socket()
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(("", self.port))
-        s.listen(5)
-        self.sock = s
-        self.running = True
-        print('Starting server.')
-
-        try:
-            self.loop()
-        finally:
-            self.stop()
-
-    def loop(self):
-        s = self.sock
-
-        poll = select.poll()
-        poll.register(s, select.POLLIN)
-
-        while True:
-            events = poll.poll(1000)
-
-            # This is where we would put periodic processing that needs
-            # to happen while waiting for requests.  But we don't have
-            # any, so all you get is this comment.
-
-            if not events:
-                continue
-
-            client, addr = s.accept()
-            print("Client connected from {}.".format(addr[0]))
-
-            try:
-                req = self.read_request(client, addr)
-                res = self.handle_request(client, req)
-            except Exception as err:
-                if isinstance(err, KeyError):
-                    status_code = 404
-                else:
-                    status_code = 500
-
-                print("ERROR: Failed handling request from {}: {}: {}"
-                      .format(addr[0], type(err), err))
-                res = Response(status_code, "text/html",
-                               "An unexpected error has occurred: {}\n".format(err))
-
-            try:
-                size = self.send_response(client, res, req)
-            except Exception as err:
-                print("ERROR: Failed sending response to {}: {}".format(addr[0], err))
-                print(
-                    '{} - - [{}] "{} {} {}" {} {}'.format(
-                        addr[0],
-                        time.time(),
-                        req.method,
-                        req.path,
-                        req.version,
-                        res.status_code,
-                        size,
-                    )
-                )
-
-            print("Closing connection from {}".format(addr[0]))
-            client.close()
-
-    def stop(self):
-        if self.sock is not None:
-            print("Closing server socket.")
-            self.sock.close()
-        self.sock = None
-        self.running = False
